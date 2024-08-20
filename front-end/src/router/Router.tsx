15806edd7f9be952870aa6224195f7c390882e86
Trung Kien <21110223@student.hcmute.edu.vn>
2024-08-20 20:54:52 +0700

    install + setup tailwindcss

--- front-end/src/router/Router.tsx
+++ front-end/src/router/Router.tsx
@@ -7,7 +7,7 @@ const Router = (props: Props) => {
   return (
     <>
     <Routes>
-      <Route path="/" element={<div>Home</div>} />
+      <Route path="/" element={<h1 className='text-3xl font-bold underline'>Home</h1>} />
     </Routes>
     </>
   )
