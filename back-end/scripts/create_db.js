1c431cc89bffc9eb939c5e902fa8bcfb19a91b22
nauht1 <thuanngo454@gmail.com>
2024-08-23 22:23:24 +0700

    (config): setup sequelize, sequelize-cli, babel, mysql2 & dotenv

---
+++ back-end/scripts/create_db.js
@@ -0,0 +1,30 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+// Load env
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+
+  const createDatabaseQuery = `CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\``;
+
+  connection.query(createDatabaseQuery, (err, result) => {
+    if (err) {
+      console.error('Error creating database:', err);
+      return;
+    }
+
+    console.log('Database created or already exists.');
+    connection.end();
+  });
+});
