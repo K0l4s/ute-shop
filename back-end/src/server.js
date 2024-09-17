79fab0de6c2c9d14b09db3fb9a2e24efa31bdb05
nauht1 <thuanngo454@gmail.com>
2024-09-17 09:07:00 +0700

    (config): cors options

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,14 +1,23 @@
 import express from "express";
 import sequelize from "./config/configdb.js";
 import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 
 const port = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.json());
 
+var corsOptions = {
+  origin: "http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
 // JWT authentication routes
-app.use('/api/auth', authRoutes);
+app.use('/api/v1/auth', authRoutes);
 
 app.listen(port, async () => {
   console.log(`Server is running on http://localhost:${port}`);
