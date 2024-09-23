c91687d2056c518cf83716695ad684b60cafbd5f
nauht1 <thuanngo454@gmail.com>
2024-09-23 17:30:48 +0700

    feat,config(client): modify redux, login, logout, navbar

--- front-end/src/components/navbar/Navbar.tsx
+++ front-end/src/components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from '../../assets/images/logo.png'
 import { TbLockAccess, TbLockAccessOff } from 'react-icons/tb'
 import { SiAwssecretsmanager } from "react-icons/si";
@@ -6,10 +7,40 @@ import { BsMenuButtonWideFill } from 'react-icons/bs';
 import { FaRegUserCircle } from 'react-icons/fa';
 import { FiShoppingCart } from 'react-icons/fi';
 import { IoMdNotificationsOutline } from 'react-icons/io';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../redux/reducers/authSlice';
+import { useNavigate, Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const isLogin = false;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
   const isAdmin = true;
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/auth/logout', {
+        method: 'POST',
+        credentials: 'include', // Gửi cookie kèm theo
+      });
+  
+      if (response.ok) {
+        // Nếu logout thành công, cập nhật trạng thái trong Redux
+        dispatch(logout());
+        alert('Đăng xuất thành công!');
+        // Có thể điều hướng về trang đăng nhập hoặc trang chủ
+        navigate('/login'); 
+      } else {
+        alert('Đăng xuất thất bại!');
+      }
+    } catch (err) {
+      console.error('Có lỗi xảy ra khi đăng xuất:', err);
+    }
+  };
+
   return (
     // navbar using tailwindcss
     <nav className="bg-gray-800 p-4 sticky top-0 ">
@@ -25,19 +56,39 @@ const Navbar = () => {
         </div>
         <div className="flex items-center">
           <ul className="flex space-x-4">
-            <li><a href="/" className="text-white">Sách</a></li>
-            <li><a href="/about" className="text-white">Nhà xuất bản</a></li>
-            <li><a href="/contact" className="text-white">Về chúng tôi</a></li>
-            <li><a href="/contact" className="text-white">Voucher</a></li>
+            <li><Link to="/" className="text-white">Sách</Link></li>
+            <li><Link to="/about" className="text-white">Nhà xuất bản</Link></li>
+            <li><Link to="/contact" className="text-white">Về chúng tôi</Link></li>
+            <li><Link to="/contact" className="text-white">Voucher</Link></li>
           </ul>
         </div>
         <div>
-          <ul className="flex space-x-4">
-            <li><a href="/cart" className="text-white"><IoMdNotificationsOutline size={30}/></a></li>
-            <li><a href="/cart" className="text-white"><FiShoppingCart size={30}/></a></li>
-            <li><a href="/login" className="text-white"><FaRegUserCircle size={30}/></a></li>
-
-          </ul>
+          {isAuthenticated ? (
+            <div>
+            <ul className="flex space-x-4">
+              <li><Link to="/cart" className="text-white"><IoMdNotificationsOutline size={30}/></Link></li>
+              <li><Link to="/cart" className="text-white"><FiShoppingCart size={30}/></Link></li>
+              <li><Link to="/account" className="text-white"><FaRegUserCircle size={30}/></Link></li>
+            </ul>
+            <div>
+              <button
+                className="bg-red-500 text-white px-4 py-2 rounded"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </div>
+            </div>
+          ) : (
+            <div className="flex space-x-4">
+              <Link to="/login" className="bg-white text-purple-600 border border-purple-600 px-4 py-2 rounded-md hover:bg-purple-600 hover:text-white transition duration-300">
+                Login
+              </Link>
+              <Link to="/register" className="bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-700 transition duration-300">
+                Register
+              </Link>
+          </div>
+          )}
         </div>
       </div>
     </nav>
