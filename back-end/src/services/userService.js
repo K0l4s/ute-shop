95755f5bda5c2d54ad7575aa1ec14dbd3fc46d5a
nauht1 <thuanngo454@gmail.com>
2024-09-26 00:44:09 +0700

    modify(server): modify some services and middleware

--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -5,20 +5,32 @@ export const getUserById = async (id) => {
 
 export const updateUserById = async (id, { firstname, lastname, address, birthday, gender, avatar_url, phone }) => {
   const user = await User.findByPk(id);
-
   if (!user) {
     throw new Error("User not found");
   }
 
-  await user.update({
+  const updatedFields = {
     firstname,
     lastname,
     address,
     birthday,
     gender,
-    avatar_url,
-    phone
-  });
+    phone,
+  };
 
-  return user;
+  if (avatar_url !== null) {
+    updatedFields.avatar_url = avatar_url;
+  }
+
+  await user.update(updatedFields);
+
+  return {
+    firstname: user.firstname,
+    lastname: user.lastname,
+    address: user.address,
+    birthday: user.birthday,
+    gender: user.gender,
+    avatar_url: user.avatar_url,
+    phone: user.phone,
+  };
 }
