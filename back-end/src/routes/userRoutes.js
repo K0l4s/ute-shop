d2915b91726695667661f4841fe0f9b26f96ce54
nauht1 <thuanngo454@gmail.com>
2024-09-26 14:03:42 +0700

    config,modify (server): config cloudinary, rm multer local storage, change update service

--- back-end/src/routes/userRoutes.js
+++ back-end/src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { authenticateJWT } from "../middlewares/authMiddleware.js";
 import { getUserProfile, updateUserProfile } from "../controllers/userController.js";
+import upload from "../config/multerConfig.js";
 
 const router = express.Router();
 
@@ -8,6 +9,6 @@ const router = express.Router();
 router.get('/profile', authenticateJWT, getUserProfile);
 
 // Cập nhật hồ sơ người dùng
-router.put('/profile/edit', authenticateJWT, updateUserProfile);
+router.put('/profile/edit', authenticateJWT, upload.single('avatar_url'), updateUserProfile);
 
 export default router;
