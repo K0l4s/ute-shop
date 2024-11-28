aec196fe6603f81db62b6546f29c0c9ab0fca952
nauht1 <thuanngo454@gmail.com>
2024-11-28 21:39:32 +0700

    fix: add migration for isReviewed

--- back-end/src/migrations/20241128143736-add-isReviewed-to-order.js
+++ back-end/src/migrations/20241128143736-add-isReviewed-to-order.js
@@ -1,22 +1,15 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Orders', 'isReviewed', {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Orders', 'isReviewed');
   }
 };
